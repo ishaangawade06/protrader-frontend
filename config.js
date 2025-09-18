31f574200e264cb367c4cef4e277d6cec65b0bb9
ishaangawade06 <ishaangawade06@gmail.com>
2025-09-18 19:44:11 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+// replace with your deployed backend base URL (https)
+const BACKEND_BASE = "https://protraderhack.onrender.com"; // e.g. https://pth-backend.onrender.com
+
+// If your backend requires an API key for admin endpoints, you can store it here
+// or set it in the browser storage via setBackendApiKey() in api.js
+// const BACKEND_API_KEY = "ENTER_IF_YOU_WANT_HARD_CODED"; // NOT recommended
