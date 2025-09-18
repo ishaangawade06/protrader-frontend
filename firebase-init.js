9c3ab6eec01d6683b254a23d8e188a64e17029d1
ishaangawade06 <ishaangawade06@gmail.com>
2025-09-18 19:48:47 +0530

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+// firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Vt0JpfAMj9uTdZHXXyot2FvB4lQ_vvE",
+  authDomain: "protraderhack-d67f0.firebaseapp.com",
+  projectId: "protraderhack-d67f0",
+  storageBucket: "protraderhack-d67f0.firebasestorage.app",
+  messagingSenderId: "1062485216321",
+  appId: "1:1062485216321:web:f34bb52a8acc0a75b24ac0"
+};
+firebase.initializeApp(firebaseConfig);
