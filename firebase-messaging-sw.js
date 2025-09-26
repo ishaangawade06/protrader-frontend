ad618af4ec8c38b0f46b0d2e79bd7057959b482a
ishaangawade06 <ishaangawade06@gmail.com>
2025-09-26 16:44:29 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,21 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyC-Vt0JpfAMj9uTdZHXXyot2FvB4lQ_vvE",
   authDomain: "protraderhack-d67f0.firebaseapp.com",
   projectId: "protraderhack-d67f0",
-  storageBucket: "protraderhack-d67f0.appspot.com",
+  storageBucket: "protraderhack-d67f0.firebasestorage.app",
   messagingSenderId: "1062485216321",
   appId: "1:1062485216321:web:f34bb52a8acc0a75b24ac0"
-};
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification.title || 'ProTraderHack';
-  const notificationOptions = {
-    body: payload.notification.body || '',
-    icon: '/icons/icon-192.png'
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  console.log("🔕 Background notification:", payload);
+  self.registration.showNotification(
+    payload.notification.title,
+    { body: payload.notification.body }
+  );
 });
